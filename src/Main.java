bbb85d076d8f6a43c1a028af89c4122315ac3625
glory1223 <hilyk1223@gmail.com>
2024-02-07 11:20:46 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    //main
+    public static void main(String[] args) {
+        /*
+        여러줄 주석 (코드에 대한 설명)
+         */
+        //sout
+        System.out.println("안녕 자바");
+
+    }
+}
---
+++ src/NewMain.java
@@ -0,0 +1,5 @@
+public class NewMain {
+    public static void main(String[] args) {
+        System.out.println("이영광입니다");
+    }
+}
